f38e2740edc6062adc4e0ba93317e14e9a1259e4
Ho Yun "Bobby <ho.chan@colorado.edu>
2018-03-29 19:53:01 -0600

    Removing Switch
    Adjust to make this circuit more basic to move forward and back. It would be better to include the switch in circuit 11.

--- Circuit_10_Motor_Basics/Circuit_10_Motor_Basics.ino
+++ Circuit_10_Motor_Basics/Circuit_10_Motor_Basics.ino
@@ -17,60 +17,33 @@ const int AIN1 = 13;           //control pin 1 on the motor driver for the right
 const int AIN2 = 12;            //control pin 2 on the motor driver for the right motor
 const int PWMA = 11;            //speed control pin on the motor driver for the right motor
 
-int switchPin = 7;             //switch to turn the robot on and off
-
 //VARIABLES
 int motorSpeed = 0;       //starting speed for the motor
 
 void setup() {
-  pinMode(switchPin, INPUT_PULLUP);   //set this as a pullup to sense whether the switch is flipped
-
   //set the motor contro pins as outputs
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
-
-  Serial.begin(9600);                       //begin serial communication with the computer
-
-  Serial.println("Enter motor speed (0-255)... ");    //Prompt to get input in the serial monitor.
 }
 
 void loop() {
-
-  if (Serial.available() > 0){          //if the user has entered something in the serial monitor
-    motorSpeed = Serial.parseInt();     //set the motor speed equal to the number in the serial message
-
-    Serial.print("Motor Speed: ");      //print the speed that the motor is set to run at
-    Serial.println(motorSpeed);
-  }
-
-  if(digitalRead(7) == LOW){            //if the switch is on...
-      spinMotor(motorSpeed);
-  } else{                               //if the switch is off...
-      spinMotor(0);                   //turn the motor off
-  }
-
-
-}
-
-/********************************************************************************/
-void spinMotor(int motorSpeed)                       //function for driving the right motor
-{
-  if (motorSpeed > 0)                                 //if the motor should drive forward (positive speed)
-  {
+    //drive motor forward (positive speed)
     digitalWrite(AIN1, HIGH);                         //set pin 1 to high
     digitalWrite(AIN2, LOW);                          //set pin 2 to low
-  }
-  else if (motorSpeed < 0)                            //if the motor should drive backwar (negative speed)
-  {
+    analogWrite(PWMA, 255);               //now that the motor direction is set, drive it at max speed
+    delay(3000);
+    
+    //drive motor backward (negative speed)
     digitalWrite(AIN1, LOW);                          //set pin 1 to low
     digitalWrite(AIN2, HIGH);                         //set pin 2 to high
-  }
-  else                                                //if the motor should stop
-  {
+    analogWrite(PWMA, 255);               //now that the motor direction is set, drive it at max speed
+    delay(3000);
+
+    //stop motor
     digitalWrite(AIN1, LOW);                          //set pin 1 to low
     digitalWrite(AIN2, LOW);                          //set pin 2 to low
-  }
-  analogWrite(PWMA, abs(motorSpeed));                 //now that the motor direction is set, drive it at the entered speed
+    analogWrite(PWMA, 0);               //now that the motor direction is set, stop motor
+    delay(3000);
 }
 
